9123b50b2298854de40e6e2df23959fc0b863aaa
DestinedCodes <yiradesat@gmail.com>
2023-09-29 00:16:23 +0100

    Setup app runs

---
+++ openapi/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///db.sqlite3'
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+        return app
