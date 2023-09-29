9bc6c893520e44ec53db7466d2bb189b5e0d622b
Kevin Koech <kevinkoech@proton.me>
2023-09-29 11:22:52 +0300

    Add doctring to register view function and checks for password and email

--- openapi/auth/routes.py
+++ openapi/auth/routes.py
@@ -7,7 +7,21 @@ auth = Blueprint("auth", __name__, url_prefix="/api/auth")
 
 @auth.route('/register', methods=['POST'])
 def register():
+    """
+    This view function facilitates registration of a new user.
 
+    This endpoint allows users to register by providing their name, email, password, and password confirmation.
+
+    Args:
+        None (Uses data from the JSON request body).
+
+    Returns:
+        JSON response:
+        - If successful: A JSON response with status 201 (Created) and user data.
+        - If there's a password confirmation mismatch: A JSON response with status 400 (Bad Request) and an error message.
+        - If the email already exists: A JSON response with status 400 (Bad Request) and an error message.
+        - If an error occurs during registration: A JSON response with status 400 (Bad Request) and an error message.
+    """
     if request.method == "POST":
         
         try:
@@ -15,11 +29,26 @@ def register():
             name = data.get("name")
             email = data.get("email")
             password = data.get("password")
+            password_confirm = data.get("password_confirm")  # New field for password confirmation
+
+            # Check if password and password confirmation match
+            if password != password_confirm:
+                return jsonify(
+                    {"status": "error",
+                     "message": "Password confirmation does not match!"
+                     }
+                ), 400
+
             hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
 
-            user = User.query.filter_by(name=name).first()
-            if user:
-                return jsonify({"message": "Alaye person don choose that name, pick another one comrade"})
+            email_exists = User.query.filter_by(email=email).first()
+            if email_exists:
+                return jsonify(
+                    {
+                    "status": "error", 
+                    "message": "Email already exists!"
+                    }
+                ), 400
 
             new_user = User(name,email,hashed_password)
             db.session.add(new_user)
@@ -44,5 +73,4 @@ def register():
                 ),
                 400,
             )
-
 
