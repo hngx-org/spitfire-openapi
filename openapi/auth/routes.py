97a5cca2da2bff74490a199f61dbb1fb1f20e121
Gachenge <gachenge1@gmail.com>
2023-09-29 11:17:20 +0300

    add a bad request status code

--- openapi/auth/routes.py
+++ openapi/auth/routes.py
@@ -17,7 +17,7 @@ def register():
             password = data.get("password")
             confirm_password = data.get("confirm_password")
             if password != confirm_password:
-                return jsonify({"Error": "Password and confirm_password do not match"})
+                return jsonify({"Error": "Password and confirm_password do not match"}), 400
             hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
 
             user = User.query.filter_by(name=name).first()
