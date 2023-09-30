b575b39253d28151f15423fa2924f1914ab36d88
Freeman-kuch <freemanoparaoch@gmail.com>
2023-09-30 09:10:21 +0100

    streaming the openai response

--- openapi/routes/interractions.py
+++ openapi/routes/interractions.py
@@ -18,17 +18,23 @@ def interractions():
 
     :return: JSON object with the response from the GPT-4 API
     """
+    # =========GETTING MOBILE JSON DATA===============
     content_type = request.headers.get('Content-Type')
     if content_type == 'application/json':
         req = request.get_json()
         prompt = req['user_input']
     else:
         return jsonify({"message": "Content-Type not supported!"}), 406
+    
+    # ===============GET USER_ID AND VERIFY CREDITS SECTION==========
 
+
+
+    # ===============USER INPUT_VALIDATION AND LIMITING=========
     text = chaaracter_validation(prompt)
 
 
-
+    #  Messages must be an array of message objects, where each object has a role (either "system", "user", or "assistant") and content. 
     messages = [
         {"role": "user", 
          "content": text
@@ -37,29 +43,33 @@ def interractions():
           "content": "you are a professional in this field"
           }
           ]
-    # this would be where the validation for the user character limit according to their credit score would be implemented
-
+    
 
     try:
-        print("response")
-        response = openai.Completion.create(
-            model="gpt-3.5-turbo-instruct",
+        response =openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
             messages=messages,
             temperature=0.6,
             stream = True
         ) 
-        content = response.choices[0]["message"]["content"].strip("\n").strip()
+        print(response)
+        # content = response.choices[0]["message"]["content"].strip("\n").strip()
+        chunk = []
+        chunk_message = []
 
         for res in response:
-            print(res)
-    except RateLimitError as Exc:
+            chunk.append(res)
+            chunk_message.append(res['choices'][0]['delta'])
+        print(chunk)
+
+    except Exception as Exc:
         print(str(Exc))
         return jsonify(content = "The server is experiencing a high volume of requests. Please try again later."), 400
 
-    return jsonify(content=content),201
+    # return jsonify(content=content),201
 
 
-# @conversation.route("/query", methods=["POST"])
+# @conversation.route("/completions", methods=["POST"])
 # def query_gpt():
 #     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
